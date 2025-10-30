9e294ce67caae05eb885208cf3a9c97299c20d9c
rbhagat32 <raghavbhagat32@gmail.com>
2025-10-31 02:30:13 +0530

    manage user + signout in sidebar-user

--- src/components/custom/sidebar/sidebar-user.tsx
+++ src/components/custom/sidebar/sidebar-user.tsx
@@ -1,9 +1,16 @@
 "use client";
 
-import { useClerk } from "@clerk/nextjs";
-import { SidebarMenuButton } from "@/components/ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { ChevronsUpDown } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { SignOutButton, useClerk } from "@clerk/nextjs";
+import { ChevronsUpDown, LogOut as LogoutIcon, User as UserIcon } from "lucide-react";
 
 export function SidebarUserInfo({
   user,
@@ -17,19 +24,46 @@ export function SidebarUserInfo({
   const { openUserProfile } = useClerk();
 
   return (
-    <SidebarMenuButton
-      size="lg"
-      className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground cursor-pointer"
-      onClick={() => openUserProfile()}
-    >
-      <Avatar className="h-8 w-8 rounded-lg">
-        <AvatarImage src={user.avatar} alt={user.name} />
-      </Avatar>
-      <div className="grid flex-1 text-left text-sm leading-tight">
-        <span className="truncate font-medium">{user.name}</span>
-        <span className="truncate text-xs">{user.email}</span>
-      </div>
-      <ChevronsUpDown className="ml-auto size-4" />
-    </SidebarMenuButton>
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground cursor-pointer"
+            >
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage src={user.avatar} alt={user.name} />
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">{user.name}</span>
+                <span className="truncate text-xs">{user.email}</span>
+              </div>
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent
+            className="w-(--radix-dropdown-menu-trigger-width) rounded-lg"
+            align="start"
+            sideOffset={6}
+          >
+            <DropdownMenuItem className="m-1 cursor-pointer" onClick={() => openUserProfile()}>
+              <UserIcon />
+              Manage account
+            </DropdownMenuItem>
+
+            <Separator />
+
+            <SignOutButton>
+              <DropdownMenuItem className="m-1 cursor-pointer">
+                <LogoutIcon />
+                Sign out
+              </DropdownMenuItem>
+            </SignOutButton>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
   );
 }
