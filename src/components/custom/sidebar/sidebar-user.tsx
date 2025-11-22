aceb93734e4dcb33751db51d4a3a7da4714484da
rbhagat32 <raghavbhagat32@gmail.com>
2025-11-22 11:02:00 +0530

    border

--- src/components/custom/sidebar/sidebar-user.tsx
+++ src/components/custom/sidebar/sidebar-user.tsx
@@ -30,7 +30,7 @@ export function SidebarUserInfo({
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton
               size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground cursor-pointer"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground cursor-pointer border"
             >
               <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user.avatar} alt={user.name} />
